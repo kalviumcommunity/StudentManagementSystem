97bf343c5bfa49d5414cbd460d7f73bec069e668
NihitYadav <nihityadav8@gmail.com>
2023-10-10 00:00:14 +0530

    .

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <string>
 
 // Define a Student class
-class Student
-{
+class Student {
 public:
     std::string name;
     int rollno;
@@ -14,36 +13,33 @@ public:
     Student(const std::string n, int r, int g) : name(n), rollno(r), Grade(g) {}
 
     // Display student information (const-qualified member function)
-    void display() const
-    {
+    void display() const {
         std::cout << "Name: " << name << "\n";
         std::cout << "RollNo: " << rollno << "\n";
         std::cout << "Grade: " << Grade << "\n";
     }
 };
 
-int main()
-{
+int main() {
     std::vector<Student> students;
 
     // Sample student data
-    students.push_back(Student("Alice", 20, 7));
-    students.push_back(Student("Bob", 13, 8));
-    students.push_back(Student("Charlie", 32, 4));
+    // students.push_back(Student("Alice", 20, 7));
+    // students.push_back(Student("Bob", 13, 8));
+    // students.push_back(Student("Charlie", 32, 4));
 
-    // // Sample student data
-    // Student student1("Alice", 20, 7);
-    // Student student2("Bob", 13, 8);
-    // Student student3("Charlie", 32, 4);
+    // Sample student data
+    Student student1("Alice", 20, 7);
+    Student student2("Bob", 13, 8);
+    Student student3("Charlie", 32, 4);
 
-    // // Add students directly to the vector
-    // students.push_back(student1);
-    // students.push_back(student2);
-    // students.push_back(student3);
+    // Add students directly to the vector
+    students.push_back(student1);
+    students.push_back(student2);
+    students.push_back(student3);
 
     // Display student information
-    for (const Student &student : students)
-    {
+    for (const Student& student : students) {
         student.display();
         std::cout << "\n";
     }
